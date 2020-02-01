4ebedf0d76b9b13b71c1eb7af562e6ecd8c635f7
Seyyid Ahmed Doğan <contact@sadogan.com>
2020-02-01 09:22:55 +0300

    some work

--- Assets/Goo.cs
+++ Assets/Goo.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Schema;
 using UnityEngine;
 
 public class Goo : MonoBehaviour {
     
-    public Rigidbody2D rb;
+    public Rigidbody2D rigid;
+    public SpriteRenderer spriteRenderer;
     public bool RightSide = false;
-    public float DashSpeedMultiplier;
-
+    public float dashSpeed;
+    public float maxWindupTime = 5f;
+    public Color windupStartColor = Color.white;
+    public Color windupChargedColor = Color.red;
+    
     public bool windupStarted = false;
+    public float windupStartTime;
     public Vector2 windupStartPos;
     public Vector2 windupLastDirection;
     
@@ -29,32 +35,41 @@ public class Goo : MonoBehaviour {
             if (!windupStarted) {
                 windupStartPos = touch.position;
                 windupStarted = true;
+                windupStartTime = Time.time;
             }
             Vector2 windupDirection = touch.position - windupStartPos;
-            windupDirection.x /= Screen.width;
-            windupDirection.y /= Screen.height;
-                
+            windupDirection = windupDirection.normalized;
             WindupDash(windupDirection); 
         }
 
         if (myTouches.Count <= 0) {
             if (windupStarted) {
-                Dash(-windupLastDirection);
+                float timePass = Time.time - windupStartTime;
+                timePass = Mathf.Min(timePass, 5f);
+                Dash(-windupLastDirection * timePass);
             }
         }
         
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
-        other.gameObject.GetComponent<Goo>().Cancel();
+        Goo g = other.gameObject.GetComponent<Goo>();
+        if (g != null) {
+            g.Cancel();
+        }
     }
 
     public void WindupDash(Vector2 direction) {
+        var timePass = Time.time - windupStartTime;
+        timePass = Mathf.Min(timePass, maxWindupTime);
+        timePass /= maxWindupTime;
+        var c = Color.Lerp(windupStartColor, windupChargedColor, timePass);
+        spriteRenderer.color = c;
         windupLastDirection = direction;
     }
     
     public void Dash(Vector2 direction) {
-        rb.velocity = direction * DashSpeedMultiplier;
+        rigid.velocity = direction * dashSpeed;
         windupStarted = false;
     }
 
